f390f23e589ffb35b50ffd7ef15ca1ff39fc9f82
Manic <basbalmanic@gmail.com>
2014-05-07 13:15:45 -0700

    Added copyright info.

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2014 Duck Syrup
+ *
+ * This file is part of CS:GO Drawing Board.
+ *
+ * CS:GO Drawing Board is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CS:GO Drawing Board is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with CS:GO Drawing Board.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 module.exports = function(app, db) {
 	//Get a user.  Does not return strats.
 	app.get('/api/:type(u|user)/:user', function(req,res) {
