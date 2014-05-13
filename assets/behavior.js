e70c6fc97f4d49fb2dae699e6ee0febd7e1432c8
Manic <basbalmanic@gmail.com>
2014-05-13 02:53:25 -0700

    Generated documentation for 280543c70e6dc8f3b2a548dcbee0b16d9a521c94

--- assets/behavior.js
+++ assets/behavior.js
@@ -6,6 +6,26 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
 
   tableOfContents = [
     {
+      "type": "file",
+      "data": {
+        "language": {
+          "nameMatchers": [".js"],
+          "pygmentsLexer": "javascript",
+          "multiLineComment": ["/*", "*", "*/"],
+          "singleLineComment": ["//"],
+          "ignorePrefix": "}",
+          "foldPrefix": "^",
+          "name": "JavaScript"
+        },
+        "sourcePath": "/Users/SeanMcGeer/Programming/ds/csgo/server.js",
+        "projectPath": "server.js",
+        "targetPath": "index",
+        "pageTitle": "index",
+        "title": "index"
+      },
+      "depth": 1,
+      "outline": []
+    }, {
       "type": "folder",
       "data": {
         "path": "apis",
@@ -159,25 +179,12 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
         }
       ]
     }, {
-      "type": "file",
+      "type": "folder",
       "data": {
-        "language": {
-          "nameMatchers": [".js"],
-          "pygmentsLexer": "javascript",
-          "multiLineComment": ["/*", "*", "*/"],
-          "singleLineComment": ["//"],
-          "ignorePrefix": "}",
-          "foldPrefix": "^",
-          "name": "JavaScript"
-        },
-        "sourcePath": "/Users/SeanMcGeer/Programming/ds/csgo/server.js",
-        "projectPath": "server.js",
-        "targetPath": "server",
-        "pageTitle": "server",
+        "path": "server",
         "title": "server"
       },
       "depth": 1,
-      "outline": [],
       "children": [
         {
           "type": "file",
