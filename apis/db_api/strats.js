ea6fd5cb4682895fbda8dd896cf6d8c8ccab3b35
Manic <basbalmanic@gmail.com>
2014-05-15 03:15:31 -0700

    Footer not header.

--- apis/db_api/strats.js
+++ apis/db_api/strats.js
@@ -87,3 +87,22 @@ function Db(username,pass) {
 exports.db = function(user,pass){
     return new Db(user,pass);
 }
+
+//CS:GO Drawing Board is a web application that allows users to develop CS:GO strategies.
+//
+//Copyright 2014 Duck Syrup
+//
+//This file is part of CS:GO Drawing Board.
+//
+//CS:GO Drawing Board is free software: you can redistribute it and/or modify
+//it under the terms of the GNU Affero General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//CS:GO Drawing Board is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU Affero General Public License for more details.
+//
+//You should have received a copy of the GNU Affero General Public License
+//along with CS:GO Drawing Board.  If not, see <http://www.gnu.org/licenses/>.
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,22 +1,3 @@
-/*
- * Copyright 2014 Duck Syrup
- *
- * This file is part of CS:GO Drawing Board.
- *
- * CS:GO Drawing Board is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * CS:GO Drawing Board is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with CS:GO Drawing Board.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 module.exports = function(app,db,utils) {
 	//Sign up
 	app.get('/signup', function(req,res) {
@@ -54,3 +35,22 @@ module.exports = function(app,db,utils) {
 		});
 	});
 }
+
+//CS:GO Drawing Board is a web application that allows users to develop CS:GO strategies.
+//
+//Copyright 2014 Duck Syrup
+//
+//This file is part of CS:GO Drawing Board.
+//
+//CS:GO Drawing Board is free software: you can redistribute it and/or modify
+//it under the terms of the GNU Affero General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//CS:GO Drawing Board is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU Affero General Public License for more details.
+//
+//You should have received a copy of the GNU Affero General Public License
+//along with CS:GO Drawing Board.  If not, see <http://www.gnu.org/licenses/>.
