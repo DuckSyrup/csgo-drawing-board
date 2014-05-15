45777f25dd1b990023a58d4b9b399a9975c87b4d
Manic <basbalmanic@gmail.com>
2014-05-15 02:06:31 -0700

    interim groc formatting change.

--- server.js
+++ server.js
@@ -1,20 +1,3 @@
-//Copyright 2014 Duck Syrup
-//
-//This file is part of CS:GO Drawing Board.
-//
-//CS:GO Drawing Board is free software: you can redistribute it and/or modify
-//it under the terms of the GNU Affero General Public License as published by
-//the Free Software Foundation, either version 3 of the License, or
-//(at your option) any later version.
-//
-//CS:GO Drawing Board is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU Affero General Public License for more details.
-//
-//You should have received a copy of the GNU Affero General Public License
-//along with CS:GO Drawing Board.  If not, see <http://www.gnu.org/licenses/>.
-
 // # server
 // Main program file--loads all APIs and determines routing.
 
@@ -140,3 +123,20 @@ console.log('Using DB username-password of ' + dbUser + '-' + dbPass);
 
 var server = app.listen(port, ip);
 io.start(server);
+
+//Copyright 2014 Duck Syrup
+//
+//This file is part of CS:GO Drawing Board.
+//
+//CS:GO Drawing Board is free software: you can redistribute it and/or modify
+//it under the terms of the GNU Affero General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//CS:GO Drawing Board is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU Affero General Public License for more details.
+//
+//You should have received a copy of the GNU Affero General Public License
+//along with CS:GO Drawing Board.  If not, see <http://www.gnu.org/licenses/>.
