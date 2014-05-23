9add3b80b540b49c357c52acb4f478423288059c
Patrick <patrick@saoconnell.com>
2014-05-22 23:54:15 -0700

    made changes to test file

--- test/db_tests.js
+++ test/db_tests.js
@@ -1,14 +1,38 @@
 var nconf = require('nconf');
-nconf.argv().file('./config.json');
-var db = require('./apis/db_api').db(nconf.get('dbuser'), nconf.get('dbpass'));
+nconf.argv().file('../config.json');
+nconf.defaults({
+	ip: 'localhost',
+	port: 8080,
+	dbuser: "user",
+	dbpass: "pass",
+	session: "secret"
+});
 
-db.newStrat({
+console.log(nconf.get('dbuser'));
+var db = require('../apis/db_api').db(nconf.get('dbuser'), nconf.get('dbpass'));
+
+//db.newStrat({
+//        stratName: "test",
+//        owner: {
+//            cat: "user",
+//            name: "bob"
+//        },
+//        map: "dust2"
+//    }, function(err, strat){
+//        console.log("fart: " + JSON.stringify(strat));
+//    });
+
+db.removeStrat({
         stratName: "test",
         owner: {
             cat: "user",
             name: "bob"
-        },
-        map: "dust2"
-    }, function(err, strat){
-        console.log(JSON.stringify(strat));
-});
+        }
+    }, function(err){
+        if(!err) {
+            console.log('done');
+        }
+        else {
+            console.log('error: ' + err);
+        }
+    });
